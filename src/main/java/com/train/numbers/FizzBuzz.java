166f962e569aa3e4c4757279c1c1b595cf887bc7
ASWIN VIJAY <aswinvi@gmail.com>
2025-04-02 20:39:33 +0530

    REFACTOR: Refactored for better readability

--- src/main/java/com/train/numbers/FizzBuzz.java
+++ src/main/java/com/train/numbers/FizzBuzz.java
@@ -2,16 +2,28 @@ package com.train.numbers;
 
 public class FizzBuzz {
 
+	private static final String BUZZ = "Buzz";
+	private static final String FIZZ = "Fizz";
+	private static final String FIZZ_BUZZ = "FizzBuzz";
+
 	public String compute(int input) {
 
-		if (input % 3 == 0 && input % 5 == 0) {
-			return "FizzBuzz";
-		} else if (input % 3 == 0) {
-			return "Fizz";
-		} else if (input % 5 == 0) {
-			return "Buzz";
+		if (isDivisibleBy3(input) && isDivisibleBy5(input)) {
+			return FIZZ_BUZZ;
+		} else if (isDivisibleBy3(input)) {
+			return FIZZ;
+		} else if (isDivisibleBy5(input)) {
+			return BUZZ;
 		}
 		return String.valueOf(input);
+	}
+
+	private boolean isDivisibleBy3(int input) {
+		return input % 3 == 0;
+	}
+
+	private boolean isDivisibleBy5(int input) {
+		return input % 5 == 0;
 	}
 
 }
